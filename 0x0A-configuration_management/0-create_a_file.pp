d54a4bbbf3fe2b18f23071eee7a8704234de630a
okibe1 <okibe4obinna@gmail.com>
2023-04-21 02:42:57 -0700

    Corrected 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Create a file
 file { '/tmp/school':
   ensure => 'file',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-} ->
+  mode   => '0744',
+  owner  => 'www-data',
+  group  => 'www-data',
+}
 
 file { '/tmp/school':
   content => 'I love Puppet',
